7ab59999597b007ccaadcedbcc0092e5805c2bb4
Jaber <jaberriyan357@gmail.com>
2024-03-21 17:12:17 +0600

    media query add

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var typed = new Typed(".text",{
-    strings:["Django Developer", "Backend Developer", "Web Developer", "Web Application Developer(Django Experties)"],
+    strings:["Web Application Developer(Django Experties)"],
     typeSpeed : 50,
     backSpeed : 30,
     backDelay : 1000,
     loop : true
 })
+
+// "Django Developer", "Backend Developer", "Web Developer", 
