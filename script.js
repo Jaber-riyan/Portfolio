a8b8e289334362716969b3ea913b658ba567af96
Jaber <jaberriyan357@gmail.com>
2024-03-21 15:35:27 +0600

    all code of portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Django Developer", "Backend Developer", "Web Developer", "Web Application Developer(Django Experties)"],
+    typeSpeed : 50,
+    backSpeed : 30,
+    backDelay : 1000,
+    loop : true
+})
