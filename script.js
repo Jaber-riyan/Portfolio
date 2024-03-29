b1b37013327badeea70bb6c0881a33ae4f60c945
Jaber <jaberriyan357@gmail.com>
2024-03-29 12:11:13 +0600

    navbar background add and responsive code write

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 var typed = new Typed(".text",{
-    strings:["Web Application Developer(Django Experties)"],
+    strings:["Django Developer", "Backend Developer", "Web Developer", "Web Application Developer(Django Experties)"],
     typeSpeed : 50,
     backSpeed : 30,
     backDelay : 1000,
     loop : true
 })
-
-// "Django Developer", "Backend Developer", "Web Developer", 
